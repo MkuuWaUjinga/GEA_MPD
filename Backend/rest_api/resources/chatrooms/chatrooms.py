1d80ae7d153f999ad819a1ff8f47b184c4915552
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-01-06 18:22:04 +0100

    Add basic implementation of getting all messages of a chat room and posting a new message to a chat room

--- Backend/rest_api/resources/chatrooms/chatrooms.py
+++ Backend/rest_api/resources/chatrooms/chatrooms.py
@@ -53,12 +53,14 @@ def entry(event, context):
                 "body": {"cows": messages}
             }
     elif http_method == 'POST':
-        expected_keys = ["chatroom_id", "message", "receiving_user_email"]
+        expected_keys = ["chatroom_id", "message", "recipient_email", "recipient_first_name", "sender_id",
+                         "sender_first_name"]
         if event.get("body") and all(key in event["body"] for key in expected_keys) and \
                 len(expected_keys)==len(event["body"]):
             try:
-                post_message(event["body"]["chatroom_id"], event["body"]["message"],
-                             event["body"]["receiving_user_eamil"], stage)
+                body = event["body"]
+                post_message(body["chatroom_id"], body["message"], body["recipient_email"],
+                             body["recipient_first_name"], body["sender_id"], body["sender_first_name"], stage)
             except (ClientError, KeyError):
                 return {
                     "statusCode": 400,
@@ -72,11 +74,32 @@ def entry(event, context):
     return response
 
 def get_all_messages(chatroom_id, stage):
+    message_table = dynamo_db_client.Table('ChatMessages'+ '-dev' if stage=='dev' else '')
 
-    return
+    messages = message_table.query(
+        KeyConditionExpression={
+            'chatRoomId = :chatroom_id'
+        },
+        ExpressionAttributeValues={
+            ":chatroom_id": chatroom_id
+        }
+    )['Items']
+    return messages_view(messages)
 
-def post_message(user_id, receiving_user_email, message, stage):
-    return
+def messages_view(messages):
+    return messages
 
-def notify_members(user_id, receiving_user_id, message):
+def post_message(chatroom_id, message, recipient_email, recipient_first_name, sender_id, sender_first_name, stage):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d:%H:%M:%S")
+    dynamo_db_client.put_item(
+        Table='ChatMessages' + '-dev' if stage == 'dev' else '',
+        Item={
+            "chatRoomId": chatroom_id,
+            "chatMessageId": f"{timestamp}#{sender_id}",
+            "message": message
+        }
+    )
+    notify_recipient(message, recipient_email, recipient_first_name, sender_first_name)
+
+def notify_recipient(message, recipient_email, recipient_first_name, sender_first_name):
     pass
