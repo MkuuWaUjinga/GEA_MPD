e9e30c573bfc1fd72cfd2c9e54964bf3e103795f
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-01-14 14:28:53 +0100

    implement storing of notification

--- Backend/analytics/analysers/notification.py
+++ Backend/analytics/analysers/notification.py
@@ -1,20 +1,48 @@
 import random
 import string
 import datetime
+import boto3
 
 class Notification:
 
-    allowed_types = ["Somatic Cell Count Alert"]
+    supported_notfication_types = ["Somatic Cell Count Alert"]
 
-    def __init__(self, type, title, escalation_status, proof):
-        if type not in self.allowed_types:
+    def __init__(self, notification_type, title, escalation_status, proof):
+        if notification_type not in self.supported_notfication_types:
             raise IllicitNotificationTypeError(f"Notification of type {type} are not supported")
         self.id = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(32)])
         self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d:%H:%M:%S")
-        self.type = type
-        self.title = title,
+        self.notification_type = notification_type
+        self.title = title
         self.escalation_status = escalation_status
         self.proof = proof
+
+    # TODO check whether notification already exists
+    # 1. Check whether notificaiton with same topic exisits
+    #   (if yes) 2. 1 Check whether cow ids are the same
+    #            (if yes): Do nothing
+    #            (if new id): Add new_id and data to existing notification (update assigned person)
+    def store_for_user(self, user_id, stage):
+        session = boto3.Session(
+            aws_access_key_id= "AKIAQIQ3QKJLYODOYXPS",
+            aws_secret_access_key="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",
+        )
+        table = session.resource('dynamodb').Table('Users'+ '-dev' if stage=='dev' else '')
+
+        table.update_item(Key={
+            'userId': user_id},
+            UpdateExpression="set notifications = list_append(notifications, :notification)",
+            ExpressionAttributeValues={
+                ':notification': [{
+                    "id": self.id,
+                    "timestamp": self.timestamp,
+                    "title": self.title,
+                    "escalation_status": self.escalation_status,
+                    "proof": self.proof
+                }]
+            }
+        )
+
 
 class IllicitNotificationTypeError(Exception):
 
