e9494378cc50d356afe9ca659e16d9323ab3cab6
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-02-01 20:51:24 +0100

    add separate frontend deployment script for local deployment

--- Backend/scripts/deploy_backend_local.sh
+++ Backend/scripts/deploy_backend_local.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Install Serverless
 cd ../Backend;
-npm install -g serverless;
 npm install;
 sls deploy --stage dev -v;
 # Deploy rest API
@@ -11,7 +10,3 @@ sls deploy --stage dev -v;
 # Deploy analytics engine
 cd ../analytics;
 sls deploy --stage dev -v;
-# Start Frontend
-cd Frontend;
-npm install;
-npm start;
---
+++ Frontend/scripts/deploy_frontend_local.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Start Frontend
+cd Frontend;
+npm install;
+npm start;
